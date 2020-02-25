b7096518c04a95abd666f9de8e7c21b62b3b47d5
terrycojones <terry@jon.es>
2020-02-25 02:32:33 +0100

    Added zip-hcov.sh script

---
+++ zip-hcov.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -Eeuo pipefail
+
+tmpdir=$(mktemp -d)
+here=$(/bin/pwd)
+zipbase=$(basename $here)-hcov
+
+mkdir $tmpdir/$zipbase
+
+for i in [DW]_*/pipelines/standard/006-hcov
+do
+    sample=$(echo $i | cut -f1 -d/)
+    mkdir $tmpdir/$zipbase/$i
+    cp $i/*.{bam,vcf}* $i/*-consensus.fasta $i/*.txt $tmpdir/$zipbase/$i
+done
+
+cd $tmpdir
+zip -r $here/$zipbase.zip .
+rm -r $tmpdir
