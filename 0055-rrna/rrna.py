fab0d6326044a43842d7a12f7ea9f1cedb4fddb9
Talitha Veith <talitha.veith@v-online.me>
2020-06-11 10:51:35 +0200

    extratMappingCoverage to search only in first column of ouput file

--- 0055-rrna/rrna.py
+++ 0055-rrna/rrna.py
@@ -86,7 +86,7 @@ def extractMappingCoverage(coverageDepth, region):
 		depthList = []
 		for line in cd:
 			for index in range(region[0], region[1]):
-				if str(index) in line:
+				if str(index) in line.split()[0]:
 					depthList.append(int(line.split()[1]))
 
 		averageCoverage = sum(depthList) / len(depthList)
